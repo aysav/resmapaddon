255d0705ae25752b1548870605098ae5f7df00ed
aysav <aysav@tut.by>
2017-06-14 16:03:25 +0300

    Meta file

---
+++ resmapaddon.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         resmapaddon
+// @category     Info
+// @version      0.3
+// @namespace    https://github.com/aysav/resmapaddon
+// @updateURL    https://raw.githubusercontent.com/aysav/resmapaddon/master/resmapaddon.meta.js
+// @downloadURL  https://raw.githubusercontent.com/aysav/resmapaddon/master/resmapaddon.js
+// @description  try to take over the world!
+// @author       You
+// ==/UserScript==
